6d69d179065988a9e6b8723be888a44556705445
jarredbama556 <jarredalldredge@Jarreds-MacBook-Pro.local>
2015-01-14 12:25:20 -0600

    Initial Commit

---
+++ Image Meta Data Viewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Image Meta Data Viewer
+//
+//  Created by Jarred Alldredge on 1/14/15.
+//  Copyright (c) 2015 Vision Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Image Meta Data Viewer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Image Meta Data Viewer
+//
+//  Created by Jarred Alldredge on 1/14/15.
+//  Copyright (c) 2015 Vision Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
